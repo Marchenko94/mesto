8509e54b1cbab9db1ccc9678ebc6997b8af6549e
Zhenya Marchenko <bartender08230@gmail.com>
2023-05-26 00:19:18 +0300

    correction

--- scripts/index.js
+++ scripts/index.js
@@ -1,118 +1,98 @@
-const buttonPopupOpened = document.querySelector('.profile__popup-opened');
-const popup = document.querySelector('.popup');
-const buttonPopupClosed = document.querySelector('.popup__closed');
-const formElement = document.querySelector('.popup__input'); 
-const nameInput = document.querySelector('.popup__input-info_type_name'); 
-const jobInput = document.querySelector('.popup__input-info_type_hobby'); 
-const title = document.querySelector('.profile__name');
-const job = document.querySelector('.profile__status');
+const buttonPopupOpened = document.querySelector('.profile__popup-opened'); 
+const popup = document.querySelector('.popup_edit-profile'); 
+const buttonPopupClosed = document.querySelector('.popup__closed'); 
+const formElement = document.querySelector('.popup__input');  
+const nameInput = document.querySelector('.popup__input-info_type_name');  
+const jobInput = document.querySelector('.popup__input-info_type_hobby');  
+const title = document.querySelector('.profile__name'); 
+const job = document.querySelector('.profile__status'); 
+const popupFullScreen = document.querySelector('.popup_full_screen'); 
+const imagePopupFullScreen = popupFullScreen.querySelector('.popup__image'); 
+const textPopupFullScreen = popupFullScreen.querySelector('.popup__text'); 
+const closePopupFullScreen = popupFullScreen.querySelector('.popup__closed'); 
+const elementsCards = document.querySelector('.elements'); 
+const templateCard = document.querySelector('.card').content; 
+const buttonPopupOpenedCards = document.querySelector('.profile__popup-opened-cards'); 
+const popupAddCards = document.querySelector('.popup_add_cards'); 
+const buttonPopupClosedCards = document.querySelector('.popup__closed-cards'); 
+const createCardButton = document.querySelector('.popup__input-button-create'); 
+const inputName = document.querySelector('.popup__input-info_type_place'); 
+const inputLink = document.querySelector('.popup__input-info_type_link');
 
+const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened') 
+buttonPopupOpened.addEventListener('click', () => { 
+    togglePopupState(popup); 
+    nameInput.value = title.textContent; 
+    jobInput.value = job.textContent; 
+} 
+); 
 
-const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened')
-
-buttonPopupOpened.addEventListener('click', () => {
-    togglePopupState(popup);
-    nameInput.value = title.textContent;
-    jobInput.value = job.textContent;
-}
-);
-
-buttonPopupClosed.addEventListener('click', () => togglePopupState(popup));
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-       title.textContent = nameInput.value;
-       job.textContent = jobInput.value;
+buttonPopupClosed.addEventListener('click', () => togglePopupState(popup)); 
+function handleFormSubmit (evt) { 
+    evt.preventDefault(); 
+       title.textContent = nameInput.value; 
+       job.textContent = jobInput.value; 
        togglePopupState(popup);
-   
+} 
+formElement.addEventListener('submit', handleFormSubmit); 
+
+// кнопка открытия(закрытия) попапа для добавления карточек 
+const togglePopupCards = (popupToToggleCards) => popupToToggleCards.classList.toggle('popup_opened'); 
+
+buttonPopupOpenedCards.addEventListener('click', () => {
+    inputName.value = '';
+    inputLink.value = '';
+    togglePopupCards(popupAddCards);
+}); 
+buttonPopupClosedCards.addEventListener('click', () => togglePopupCards(popupAddCards)); 
+
+//добавление и сохранение карточки  
+createCardButton.addEventListener('click', (event) => { 
+    event.preventDefault(); 
+    addCardsToTemplate(inputName.value, inputLink.value); 
+    togglePopupCards(popupAddCards);
+}) 
+//добавление карточек через template 
+function addCardsToTemplate(name, link) {
+    const itemElement = templateCard.cloneNode(true); 
+    itemElement.querySelector('.element__text').innerText = name;
+    itemElement.querySelector('.element__image').src = link; 
+    itemElement.querySelector('.element__image').alt = name;
+    addLike(itemElement);
+    openFullScreenImage(itemElement);
+    removeCard(itemElement);
+    elementsCards.insertBefore(itemElement, elementsCards.firstChild); 
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+initialCards.forEach((item) => { 
+    const itemElement = addCardsToTemplate(item.name, item.link);
+}); 
 
-
-
-// кнопка открытия(закрытия) попапа для добавления карточек
-const buttonPopupOpenedCards = document.querySelector('.profile__popup-opened-cards');
-const popupAddCards = document.querySelector('.popup_add_cards');
-const buttonPopupClosedCards = document.querySelector('.popup__closed-cards');
-
-const togglePopupCards = (popupToToggleCards) => popupToToggleCards.classList.toggle('popup_opened');
-
-buttonPopupOpenedCards.addEventListener('click', () => togglePopupCards(popupAddCards));
-
-buttonPopupClosedCards.addEventListener('click', () => togglePopupCards(popupAddCards));
-
-
-//добавление и сохранение карточки
-const createCardButton = document.querySelector('.popup_add_cards .popup__input-button');
-createCardButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    const inputName = document.querySelector('.popup_add_cards .popup__input-info_type_name');
-    const inputLink = document.querySelector('.popup_add_cards .popup__input-info_type_hobby');
-    addCardsToTemplate(inputName.value, inputLink.value);
-})
-
-
-//добавление карточек через template
-const elementsCards = document.querySelector('.elements');
-const templateCard = document.querySelector('.card').content;
-
-
-initialCards.forEach((item) => {
-  const itemElement = templateCard.cloneNode(true);
-  itemElement.querySelector('.element__text').innerText = item.name;
-  itemElement.querySelector('.element__image').src = item.link;
-  
-//возможность ставить лайк этим карточкам
-  itemElement.querySelector('.element__like').addEventListener('click', function(event){
-    event.target.classList.toggle('element__like_active');
- })
- elementsCards.append(itemElement);
-});
-
-//добавление новый карточек
-function addCardsToTemplate (name, link) {
-    const templateCard = document.querySelector('.card').content;
-    const cloneCard = templateCard.cloneNode(true);
-    const cloneCardImage = cloneCard.querySelector('.element__image');
-    const cloneCardText = cloneCard.querySelector('.element__text');
-    const elementsCards = document.querySelector('.elements');
-    
-//воззможность им ставить лайк
-    cloneCard.querySelector('.element__like').addEventListener('click', function(event){
-        event.target.classList.toggle('element__like_active');
-     })
-
-    cloneCardText.textContent = name;
-    cloneCardImage.src = link;
-    elementsCards.insertBefore(cloneCard, elementsCards.firstChild);
-    togglePopupCards(popupAddCards); 
-    openFullScreenImage();
+function addLike(itemElement){
+    itemElement.querySelector('.element__like').addEventListener('click', function(event){ 
+    event.target.classList.toggle('element__like_active'); 
+  }) 
+}
+function removeCard(itemElement) {
+    const basketButton = itemElement.querySelector('.element__basket-button'); 
+    basketButton.addEventListener('click', () => { 
+    basketButton.closest('.element').remove();
+}) 
 }
 
-const popupFullScreen = document.querySelector('.popup_full_screen');
-const imagePopupFullScreen = popupFullScreen.querySelector('.popup__image');
-const textPopupFullScreen = popupFullScreen.querySelector('.popup__text');
-const closePopupFullScreen = popupFullScreen.querySelector('.popup__closed');
+function openFullScreenImage(itemElement) { 
+    const image = itemElement.querySelector('.element__image'); 
+    const titleCaption = itemElement.querySelector('.element__text'); 
+    image.addEventListener('click', () => { 
+    imagePopupFullScreen.src = image.src; 
+    textPopupFullScreen.textContent = titleCaption.textContent; 
+    popupFullScreen.classList.add('popup_opened'); 
+}); 
+} 
 
-function openFullScreenImage() {
-    const allCards = document.querySelectorAll('.element');
-    allCards.forEach((card) => {
-        const image = card.querySelector('.element__image');
-        const titleCaption = card.querySelector('.element__text');
-        const basketButton = card.querySelector('.element__basket-button');
-        basketButton.addEventListener('click', () => {
-            card.remove();
-        })
-        image.addEventListener('click', () => {
-            imagePopupFullScreen.src = image.src;
-            textPopupFullScreen.textContent = titleCaption.textContent;
-            popupFullScreen.classList.add('popup_opened');
-        });
-    })
-    closePopupFullScreen.addEventListener('click', () => {
-        popupFullScreen.classList.remove('popup_opened');
-    });
+function closePopupFullScreenModal() {
+    closePopupFullScreen.addEventListener('click', () => { 
+    popupFullScreen.classList.remove('popup_opened'); 
+}); 
 }
-openFullScreenImage();
-
-
+closePopupFullScreenModal()
